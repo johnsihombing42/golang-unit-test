921620ec9ec2c4d26cc223448d01b1cc5fc886aa
johnsihombing42 <johnsihombing42@gmail.com>
2023-02-06 20:58:56 +0700

    update

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,9 +1,12 @@
 package helper
 
 import (
+	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestHelloWorld(t *testing.T){
@@ -18,4 +21,20 @@ func TestHelloWorld(t *testing.T){
 func TestHelloWorldAssert(t *testing.T){
 	result:=HelloWorld("John")
 	assert.Equal(t,"Hello John",result,"Result must be Hello John")
+}
+
+func TestSkip(t *testing.T){
+	if runtime.GOOS=="darwin"{
+		t.Skip("Can not run on mac os")
+	}
+	result:=HelloWorld("John")
+	require.Equal(t,"Hello John",result,"Result must be Hello John")
+}
+
+func TestMain(m *testing.M){
+	//before
+	fmt.Println("Before Unit Test")
+	m.Run()
+	//After
+	fmt.Println("After Unit Test")
 }
